a8a45e0c75ec80c8d4681e531c5fd8f843b26399
linhnguyen0909 <nguyenhoailinh9099@gmail.com>
2022-04-26 22:50:16 +0700

    feat: crud customer

---
+++ app/Actions/Customer/ShowListCustomerAction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\Customer;
+
+use App\Models\User;
+use App\Repositories\Customer\CustomerRepository;
+use App\Supports\Traits\HasTransformer;
+use App\Transformers\Customer\CustomerTransformer;
+use App\Transformers\UserTransformer;
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class
+ * @package App\Actions\User
+ */
+class ShowListCustomerAction
+{
+    use HasTransformer;
+
+
+    protected CustomerRepository $customerRepository;
+
+    public function __construct(CustomerRepository $customerRepository)
+    {
+        $this->customerRepository = $customerRepository;
+    }
+
+
+    /**
+     * @return JsonResponse
+     */
+    public function __invoke()
+    {
+        $response = $this->customerRepository->paginate();
+
+        return $this->httpOK($response, CustomerTransformer::class);
+    }
+}
